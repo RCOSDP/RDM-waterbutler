ba49a6227466d420a182171f53f64f5d84418227
tma-ndhuy <ndhuy@tma.com.vn>
2023-03-08 13:51:42 +0700

    refs (C).2.6 [機関ストレージ]GakuNin RDM ファイル機能の既存のバグ修正: Fix bug IT 02

--- waterbutler/server/utils.py
+++ waterbutler/server/utils.py
@@ -130,6 +130,7 @@ class UtilMixin:
                 # Temp fix, write does not accept bytearrays currently
                 if isinstance(chunk, bytearray):
                     chunk = bytes(chunk)
+                self.set_header('Content-Length', len(chunk))
                 self.write(chunk)
                 self.bytes_downloaded += len(chunk)
                 del chunk
